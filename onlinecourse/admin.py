d203ababfe36e035a2f56c482028133f417d815b
francisco-sudo <diazleyva_fcojavier@hotmail.com>
2021-11-22 00:12:44 +0000

    changes made from theia for final project

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
+from .models import Choice, Question, Submission
 from .models import Course, Lesson, Instructor, Learner
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
-
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4    
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -28,3 +34,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
